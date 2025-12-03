b1adc0a4b5497facea9ffce0026f89ac931efeb4
Hakan <Hakankarimpour@gmail.com>
2025-12-03 23:05:08 +0330

    dekete comment (js)

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 document.querySelector(".Resume").addEventListener("click", function () {
   document.querySelector(".me").style.display = "none";
   document.querySelector(".Me").style.backgroundColor = "#f5f5f5";
-  //   document.querySelector(".faAngleRight").style.fontSize = "18px";
   document.querySelector(".resume").style.display = "block";
   document.querySelector(".Resume").style.backgroundColor = "pink";
 });
